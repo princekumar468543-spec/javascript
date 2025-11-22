33ed80ffb0c8881a2cf0e1cdd9579120f99e6fc0
princekumar468543-spec <princekumar468543@gmail.com>
2025-11-22 11:15:06 +0530

    Add files via upload

---
+++ fifth.js
@@ -0,0 +1,155 @@
+/* 
+let x = 5;
+let y = 6;
+console.log("x + y=", x + y);
+console.log("x - y=", x - y);
+console.log("x*y=", x*y);
+console.log("x/y=",x/y);
+
+
+let a = 12;
+a += 3;
+a *= 2;
+a -= 11;
+a  %= 3;
+
+console.log("a= ",a);
+
+// practise question
+
+1.
+let x = 10;
+let y = 20;
+
+if (x > 5 && y < 25) {
+    console.log("a");
+} else {
+    console.log("b");
+}
+
+2.
+
+let isAdmin = true;
+let islogged = false;
+
+if (isAdmin || isLoggedIn) {
+    console.log("access granted");
+} else {
+    console.log("Acces denied");
+}
+
+3.
+let a1 = 0;
+
+if (a1) {
+    console.log("truthy");
+} else {
+    console.log("falsy");
+}
+
+4.
+let score = 78;
+let grade = score >= 90 ? "a" : score >= 75 ? "b" :score >= 60 ? "c" : "fail";
+console.log(grade);
+5.
+let points = 60;
+let status = points > 100 ? "gold" :points > 50 ? "silver" : "bronze";
+
+console.log(status);
+6.
+let LoggedIn = true;
+let hastoken = true;
+let access = LoggedIn && hastoken ? "allow" : "deny";
+console.log(access);
+7.
+let f = 3;
+let g = f++;
+console.log(f,g);
+8.
+let p = 5;
+let q = ++p;
+console.log(p,q);
+9.
+let m = 10;
+console.log(m--);
+console.log(m);
+10. 
+let n = 5;
+let result = n++ + ++n;
+console.log(result);
+11
+let count = 5;
+
+if (count-- ===5) {
+    console.log('matched');
+} else {
+    console.log("nit maatched");
+}
+    
+
+12
+let a2 = 5;
+let y2 = 3 ;
+let z = 2;
+console.log("a2+y2*z =",a2+y2*z);
+13
+let a3 = 10;
+let y3 = 4;
+console.log("a3%y3 =",a3%y3);
+
+14
+let a4 = 20;
+let y4 = 0;
+console.log("a4/y4 =", a4/y4);
+
+15
+let a5 = 2 ;
+let y5 = 3 ;
+console.log("a5**y5 =", a5**y5);
+16
+let a6 = 7;
+let y6 = 3;
+let a7 = 2;
+let y7 = 3;
+console.log("a6-y6 + a7*y7 =",a6-y6 + a7*y7);
+17
+let a8 = 5;
+let y8 = "5";
+console.log("a8==y8",a8==y8);
+18
+let a9 = 5;
+let y9 = "5"
+console.log("a9===y9",a9===y9);
+19
+let a10 = 10;
+let y10 = 20;
+console.log("a10>y10 =",a10>y10);
+20
+let a11 = 3;
+let y11 = 3;
+console.log("a11<=y11 =",a11<=y11);
+*/
+
+21
+let y1 = "2";
+let y2 = 12;
+console.log("y1 > y2 = ",y1>y2);
+22.
+let y3 = true;
+let y4 = false;
+console.log("y3&&y4 =",y3&&y4);
+23.
+let y5 = true;
+let y6 = false;
+console.log("y5||y6 = ",y5||y6);
+24.
+let y7 = false;
+let y8 = false;
+console.log("!y7 y8 = ",!y7 ,!y8);
+25.
+let y9 = 5;
+let y10 = 3;
+ let y11 = 2;
+ let y12 = 3;
+ console.log("y9>y10 && y11>y12 =",y9>y10 && y11>y12);
+

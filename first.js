d3006b719e33873d7194047dd65b3cf19ac75fa5
princekumar468543-spec <princekumar468543@gmail.com>
2025-11-24 21:54:03 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,36 @@
+console.log("operators in js")
+let a = 45;
+let b = 4;
+console.log("a + b =", a+b)
+console.log("a - b =",a-b)
+console.log("a - b =", a-b)
+console.log("a / b =",a/b)
+console.log("a ** b =" ,a**b)
+console.log("a % b=", a%b)
+console.log("++a = ", ++a)
+console.log("a++ =", a++)
+console.log("--a =",--a)
+console.log("a-- =", a-- )
+console.log("a =",a)
+console.log("a-- =", a--)
+
+// Assigment operators//
+let assigmment = 1;
+assigmment += 5
+console.log(assigmment)
+
+// comperision operator//
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1==comp2",comp1==comp2)
+console.log("comp1 != comp2", comp1!=comp2)
+console.log("comp1 ===comp2" , comp1===comp2)
+console.log("comp1 !== comp2 is", comp1!== comp2) 
+
+// logical opertors//
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+
+let a1= prompt("Hey whats you age")
---
+++ second.js
@@ -0,0 +1,19 @@
+// first question//
+let a = 20
+let b = 4
+console.log("a + b", a + b)
+console.log("a - b",a - b)
+console.log("a * b", a*b)
+console.log("a/b" , a/b)
+// second question//
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastname = "Baggins";
+
+console.log("num + str",num + str)
+console.log("num + str2",num + str2)
+console.log("num +isPresent",num + isPresent)
+console.log("firstName + lastname",firstName + lastname)
